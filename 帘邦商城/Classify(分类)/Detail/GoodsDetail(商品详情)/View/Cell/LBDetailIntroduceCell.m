c134d0c034e9d79d455bdbeabc13106fadf86ce7
LeeDuanGG <lgr0207@163.com>
2018-06-05 10:35:45 +0800

    6-4号更新

--- 帘邦商城/Classify(分类)/Detail/GoodsDetail(商品详情)/View/Cell/LBDetailIntroduceCell.m
+++ 帘邦商城/Classify(分类)/Detail/GoodsDetail(商品详情)/View/Cell/LBDetailIntroduceCell.m
@@ -25,4 +25,7 @@
 
 @implementation LBDetailIntroduceCell
 
+
+
+
 @end
---
+++ 帘邦商城/Classify(分类)/Detail/GoodsDetail(商品详情)/View/Cell/LBDetailTwoCell.h
@@ -0,0 +1,13 @@
+//
+//  LBDetailTwoCell.h
+//  帘邦商城
+//
+//  Created by apple on 2018/6/4.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBDetailTwoCell : UICollectionViewCell
+
+@end
